607fac6da65b4c9decf24762d67e99b4b4ba8eb6
Ben Parman <benparman@gmail.com>
2018-11-25 21:29:19 -0700

    Add button with function to setState() for gameNumber to a random number between 1 and 100.

---
+++ src/components/gameScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function newGame(props) {
+
+  return (
+    <button onClick={e => e.preventDefault()}>
+      Click Me
+    </button>
+  )
+
+}
--- src/components/mainScreen.js
+++ src/components/mainScreen.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
-import gameScreen from './gameScreen';
-import guessedNumbers from './guessedNumbers';
-import guessInput from './guessInput';
-import msgBox from './msgBox';
+// import gameScreen from './gameScreen';
+// import guessedNumbers from './guessedNumbers';
+// import guessInput from './guessInput';
+// import msgBox from './msgBox';
+// import NewGame from './gameScreen';
 
-export default class mainScreen extends React.component {
+export default class MainScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      gameNumber: 55
+      gameNumber: 55,
+      userGuess: 50,
+      guessCount: 0,
+      guessedNumbers: []
     }
   }
+
+  generateNumber = () => {
+    const number = Math.random()*100;
+    this.setState({gameNumber: number})
+  }
+
   render() {
     const gameNumber = this.state.gameNumber;
     console.log(<h1>"The current number is: "{gameNumber}</h1>)
@@ -19,8 +29,14 @@ export default class mainScreen extends React.component {
     return (
       <div className="mainScreen">
         <h2>The current number is : {gameNumber}</h2>
+        <button onClick={e => {
+          e.preventDefault();
+          const newNum = Math.ceil(Math.random().toFixed(2)*100);
+          console.log('This is newNum: ', newNum);
+          this.setState({gameNumber: newNum})
+        }}>Generate New Number</button>
       </div>
-    )
+    );
   }
 }
 
